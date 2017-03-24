6224be6a95199a3f99f4138646fbe4f0334f8d6c
Marko Stanoevich <MStanoevich@gmail.com>
2017-03-24 13:44:48 +0100

    Advanced Driving 3.0
    If no direction is pressed it stops suddenly

--- ARDUINO/car/advanced_driving.ino
+++ ARDUINO/car/advanced_driving.ino
@@ -39,46 +39,46 @@ void loop() {
     rData = Serial.read();
   
     if(rData == 'q'){         //diagonal forward left turn
-      int lSpeed = 20;
-      int rSpeed = 50;
+      lSpeed = 20;
+      rSpeed = 50;
       moveFor(lSpeed,rSpeed);
     }
     if(rData == 'e'){       //diagonal forward right turn
-      int lSpeed = 50;
-      int rSpeed = 20;
+      lSpeed = 50;
+      rSpeed = 20;
       moveFor(lSpeed, rSpeed);
     }
     if(rData == 'c'){      //diagonal backward right turn
-      int lSpeed = -50;
-      int rSpeed = -20;
+      lSpeed = -50;
+      rSpeed = -20;
       moveBack(lSpeed, rSpeed);
     }
     if(rData == 'z'){     //diagonal backward left turn
-      int lSpeed = -20;
-      int rSpeed = -50;
+      lSpeed = -20;
+      rSpeed = -50;
       moveBack(lSpeed, rSpeed);
     }
     if(rData == 'w'){                  //move forward
-      int lSpeed = 50;
-      int rSpeed = 50;
+      lSpeed = 50;
+      rSpeed = 50;
       moveFor(lSpeed, rSpeed);
     }
     else if(rData == 's'){            //move backward
-      int lSpeed = -50;
-      int rSpeed = -50;
+      lSpeed = -50;
+      rSpeed = -50;
       moveBack(lSpeed, rSpeed);
     }
     else if(rData == 'a'){            //turn in-place to the left (more of a drift in place)
-      int lSpeed = -50;
-      int rSpeed = 50;
+      lSpeed = -50;
+      rSpeed = 50;
       turn(lSpeed, rSpeed);
     }
     else if(rData == 'd'){            //turn in-place to the right (more of a drift in place)
-      int lSpeed = 50;
-      int rSpeed = -50;
+      lSpeed = 50;
+      rSpeed = -50;
       turn(lSpeed, rSpeed);
     }
-    else if(rData == 'x'){            //turn in-place to the right (more of a drift in place)
+    else if(rData == 'x'){            //stops
       eStop(lSpeed, rSpeed);
     }
   }
