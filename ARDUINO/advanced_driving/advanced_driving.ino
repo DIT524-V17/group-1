c8786d9ffabbb4ad54948300cab1000c42df0a23
M. Murat Kan <gusmehmka@student.gu.se>
2017-03-31 15:50:47 +0200

    changed read data from char to string

--- ARDUINO/advanced_driving/advanced_driving.ino
+++ ARDUINO/advanced_driving/advanced_driving.ino
@@ -1,7 +1,5 @@
 #include <Smartcar.h>
 
-//char rData;
-
 const int TRIGER_ODOL_PIN = 2;
 const int TRIGER_ODOR_PIN = 3;
 const int TRIGGER_PIN = 6;
@@ -11,60 +9,42 @@ Car car;
 
 Odometer odoLeft;
 Odometer odoRight;
-
 SR04 US;
-int lSpeed = 15;
-int rSpeed = 15;
-char rData[3];
-int S;
 
-void increaseSpeed() {
-  while (lSpeed < 99 && rSpeed < 99) {
-    lSpeed += 5;
-    rSpeed += 5;
-    break;
-  }
+int lSpeed;
+int rSpeed;
+String rData;
+
+// to split up the string and set 1. and 2. char of it as speed
+// Note: since we change from to read char to string it takes almost 2 sec for any movment
+// probably we need to fix this...
+
+int extract() {
+  int carSpeed = rData.substring(1, 3).toInt();
+  return carSpeed;
 }
 
-void decreaseSpeed() {
-  while (lSpeed > 15 && rSpeed > 15) {
-    lSpeed -= 5;
-    rSpeed -= 5;
-    break;
-  }
-}
-void extract() {
-  String Stemp = rData[1] + rData[2] + "";
-  int S = Stemp.toInt();
-  //return S;
-  //for (int i = 0; i < 3; i = i ++) {
-  //rData[i] = Serial.read();
-}
-
-
-void avarageSpeed() {
-  //float avarage = (odoLeft.getSpeed() + odoRight.getSpeed())/2;
-  //Serial.print(avarage);
-}
 void moveFor(int lSpeed, int rSpeed) {
   car.setMotorSpeed(lSpeed, rSpeed);
-  avarageSpeed();
-
 }
 
 void moveBack(int lSpeed, int rSpeed) {
   car.setMotorSpeed(lSpeed, rSpeed);
-  avarageSpeed();
 }
 void turn(int lSpeed, int rSpeed) {
   car.setMotorSpeed(lSpeed, rSpeed);
-  avarageSpeed();
 }
 void eStop(int lSpeed, int rSpeed) {
   car.setMotorSpeed(-lSpeed, -rSpeed);
   delay(100);
   car.setMotorSpeed(0, 0);
-  avarageSpeed();
+}
+
+// to send current speed to controller
+// maybe it needs to be improved
+void avarageSpeed() {
+  float avarage = (odoLeft.getSpeed() + odoRight.getSpeed()) / 2;
+  Serial.println(avarage);
 }
 
 void setup() {
@@ -79,134 +59,70 @@ void setup() {
 
 void loop() {
 
+  avarageSpeed();
+
   if (Serial.available() > 0) {
 
+    rData = Serial.readString();
+    char y = rData.charAt(0);
 
-    for (int i = 0; i < 3; i = i ++) {
-      rData[i] = Serial.read();
-    }
-    char y = rData[0];
-    extract();
     switch (y) {
       case 'w' :                         //move forward
-        lSpeed = S;
-        rSpeed = S;
+        lSpeed = extract();
+        rSpeed = extract();
         moveFor(lSpeed, rSpeed);
         break;
 
       case 's' :                        //move backward
-        lSpeed = -S;
-        rSpeed = -S;
+        lSpeed = -extract();
+        rSpeed = -extract();
         moveBack(lSpeed, rSpeed);
         break;
 
       case 'a' :                       //turn in-place to the left (more of a drift in place)
-        lSpeed = -S;
-        rSpeed = S;
+        lSpeed = -extract();
+        rSpeed = extract();
         turn(lSpeed, rSpeed);
         break;
 
       case 'd' :                      //turn in-place to the right (more of a drift in place)
-        lSpeed = S;
-        rSpeed = -S;
+        lSpeed = extract();
+        rSpeed = -extract();
         turn(lSpeed, rSpeed);
         break;
 
       case 'q' :                     //diagonal forward left turn
-        lSpeed = S / 2;
-        rSpeed = S;
+        lSpeed = extract() / 2;
+        rSpeed = extract();
         moveFor(lSpeed, rSpeed);
         break;
 
       case 'e' :                    //diagonal forward right turn
-        lSpeed = S;
-        rSpeed = S / 2;
+        lSpeed = extract();
+        rSpeed = extract() / 2;
         moveFor(lSpeed, rSpeed);
         break;
 
-
       case 'z' :                  //diagonal backward left turn
-        lSpeed = -(S / 2);
-        rSpeed = -S;
+        lSpeed = -(extract() / 2);
+        rSpeed = -extract();
         moveBack(lSpeed, rSpeed);
         break;
 
       case 'c' :                 //diagonal backward right turn
-        lSpeed = -S;
-        rSpeed = -(S / 2);
+        lSpeed = -extract();
+        rSpeed = -(extract() / 2);
         moveBack(lSpeed, rSpeed);
         break;
 
       case 'x' :                //Stop
-        eStop(lSpeed, rSpeed);
+        eStop(extract(), extract());
         break;
 
       default :
         Serial.print("The smartCar doesn't move!");
         break;
-
-
-
     }
   }
 }
-/* if(rData[0] == 'q'){         //diagonal forward left turn
-   lSpeed = S/2;
-   rSpeed = S;
-   moveFor(lSpeed,rSpeed);
-
-  }
-  if(rData[0] == 'e'){       //diagonal forward right turn
-   lSpeed = S;
-   rSpeed = S/2;
-   moveFor(lSpeed, rSpeed);
-  }
-  if(rData[0] == 'c'){      //diagonal backward right turn
-   lSpeed = -S;
-   rSpeed = -(S/2);
-   moveBack(lSpeed, rSpeed);
-  }
-  if(rData[0] == 'z'){     //diagonal backward left turn
-   lSpeed = -(S/2);
-   rSpeed = -S;
-   moveBack(lSpeed, rSpeed);
-  }
-  if(rData[0]== 'w'){                  //move forward
-   lSpeed = S;
-   rSpeed = S;
-   moveFor(lSpeed, rSpeed);
-  }
-  else if(rData[0] == 's'){            //move backward
-   lSpeed = -S;
-   rSpeed = -S;
-   moveBack(lSpeed, rSpeed);
-  }
-  else if(rData[0] == 'a'){            //turn in-place to the left (more of a drift in place)
-   lSpeed = -S;
-   rSpeed = S;
-   turn(lSpeed, rSpeed);
-  }
-  else if(rData[0] == 'd'){            //turn in-place to the right (more of a drift in place)
-   lSpeed = S;
-   rSpeed = -S;
-   turn(lSpeed, rSpeed);
-  }
-  else if(rData[0] == 'x'){            //stops
-   eStop(lSpeed, rSpeed);
-  }
-  }
-   rData = Serial.read();
-
-  //    if(rData == 'm'){
-  //      increaseSpeed();
-  //      Serial.println(lSpeed);
-  //      moveFor(lSpeed,rSpeed);
-  //
-  //    }
-  //
-  //    if(rData == 'k'){
-  //      decreaseSpeed();
-  //      Serial.println(lSpeed);
-  //      moveFor(lSpeed,rSpeed);
-  }*/
 
