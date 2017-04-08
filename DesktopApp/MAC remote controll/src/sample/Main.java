c304282ce247764771f707dd920b40a48753a1d0
Samer Daknache <sd@skip.local>
2017-04-08 12:22:44 +0200

    Updated the GUI to a joystick, the integration is not complete.

--- DesktopApp/MAC remote controll/src/sample/Main.java
+++ DesktopApp/MAC remote controll/src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
@@ -29,6 +30,11 @@ public class Main extends Application {
     String url = "btspp://201510201409:1";
     final RemoteDevice[] hc05device = new RemoteDevice[1];
     final boolean[] scanFinished = {false};
+
+    //declare gui variables
+    Boolean moveCircle = false;
+    Circle outerCircle = new Circle();
+    Circle innerCircle = new Circle();
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -80,193 +86,87 @@ public class Main extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, 500, 600);
         primaryStage.setScene(scene);
-
         primaryStage.setTitle("MAC remote controller");
-
         Group g = new Group();
-
-        UpTriangle.getPoints().addAll(new Double[]{
-                260.0, 170.0, //top
-                200.0, 250.0, //left
-                320.0, 250.0  //right
+        innerCircle.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                moveCircle = true;
+            }
         });
-
+        innerCircle.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getX() < (outerCircle.getCenterX() + innerCircle.getRadius() * 2) && mouseEvent.getX() > (outerCircle.getCenterX() - innerCircle.getRadius() * 2) &&
+                        mouseEvent.getY() < (outerCircle.getCenterY() + innerCircle.getRadius() * 2) && mouseEvent.getY() > (outerCircle.getCenterY() - innerCircle.getRadius() * 2)){
+                    innerCircle.setCenterX(mouseEvent.getX());
+                    innerCircle.setCenterY(mouseEvent.getY());
+                }
+            }
+        });
+        innerCircle.setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                moveCircle = false;
+                innerCircle.setCenterX(130);
+                innerCircle.setCenterY(90);
+            }
+        });
+        outerCircle.setCenterX(130);
+        outerCircle.setCenterY(90);
+        outerCircle.setRadius(60);
+        innerCircle.setCenterX(130);
+        innerCircle.setCenterY(90);
+        innerCircle.setRadius(20);
+        outerCircle.setFill(Color.GREEN);
+        innerCircle.setFill(Color.BLACK);
+        g.getChildren().add(outerCircle);
+        g.getChildren().add(innerCircle);
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
-                switch (keyEvent.getCode()){
+                switch (keyEvent.getCode()) {
                     case W:
-                        UpTriangle.setFill(Color.RED);
-                        try {
-                            os.write("w".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterY(outerCircle.getCenterY() - innerCircle.getRadius() * 2);
                         break;
                     case S:
-                        DownTriangle.setFill(Color.RED);
-                        try {
-                            os.write("s".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterY(outerCircle.getCenterY() + innerCircle.getRadius() * 2);
                         break;
                     case D:
-                        RightTriangle.setFill(Color.RED);
-                        try {
-                            os.write("d".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterX(outerCircle.getCenterX() + innerCircle.getRadius() * 2);
                         break;
                     case A:
-                        LeftTriangle.setFill(Color.RED);
-                        try {
-                            os.write("a".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterX(outerCircle.getCenterX() - innerCircle.getRadius() * 2);
                         break;
-
                 }
+                double x = outerCircle.getCenterX() - innerCircle.getCenterX();
+                double y = outerCircle.getCenterY() - innerCircle.getCenterY();
+                double angle = Math.atan2(y,x);
+                System.out.println(Math.toDegrees(angle));
             }
         });
-
         scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
-                switch (keyEvent.getCode()){
+                switch (keyEvent.getCode()) {
                     case W:
-                        UpTriangle.setFill(Color.BLACK);
-                        try {
-                            os.write("x".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterY(90);
                         break;
                     case S:
-                        DownTriangle.setFill(Color.BLACK);
-                        try {
-                            os.write("x".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterY(90);
                         break;
                     case D:
-                        RightTriangle.setFill(Color.BLACK);
-                        try {
-                            os.write("x".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterX(130);
                         break;
                     case A:
-                        LeftTriangle.setFill(Color.BLACK);
-                        try {
-                            os.write("x".getBytes());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        innerCircle.setCenterX(130);
                         break;
                 }
             }
         });
-
-
-
-        UpTriangle.setOnMousePressed(new EventHandler<MouseEvent>()
-        {
-            @Override
-            public void handle(MouseEvent t) {
-                UpTriangle.setFill(Color.RED);
-            }
-        });
-
-        UpTriangle.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                UpTriangle.setFill(Color.BLACK);
-            }
-        });
-
-        DownTriangle.getPoints().addAll(new Double[]{
-                260.0, 420.0, //top
-                200.0, 350.0, //left
-                320.0, 350.0  //right
-        });
-
-        DownTriangle.setOnMousePressed(new EventHandler<MouseEvent>()
-        {
-            @Override
-            public void handle(MouseEvent t) {
-                DownTriangle.setFill(Color.RED);
-            }
-        });
-
-        DownTriangle.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                DownTriangle.setFill(Color.BLACK);
-            }
-        });
-
-
-        RightTriangle.getPoints().addAll(new Double[]{
-                410.0, 300.0, //top
-                330.0, 250.0, //left
-                330.0, 350.0  //right
-        });
-
-        RightTriangle.setOnMousePressed(new EventHandler<MouseEvent>()
-        {
-            @Override
-            public void handle(MouseEvent t) {
-                RightTriangle.setFill(Color.RED);
-            }
-        });
-
-        RightTriangle.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                RightTriangle.setFill(Color.BLACK);
-            }
-        });
-
-
-
-        LeftTriangle.getPoints().addAll(new Double[]{
-                120.0, 300.0, //top
-                190.0, 250.0, //left
-                190.0, 350.0  //right
-        });
-
-        LeftTriangle.setOnMousePressed(new EventHandler<MouseEvent>()
-        {
-            @Override
-            public void handle(MouseEvent t) {
-                LeftTriangle.setFill(Color.RED);
-            }
-        });
-
-        LeftTriangle.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                LeftTriangle.setFill(Color.BLACK);
-            }
-        });
-
-
-        g.getChildren().add(UpTriangle);
-        g.getChildren().add(DownTriangle);
-        g.getChildren().add(RightTriangle);
-        g.getChildren().add(LeftTriangle);
-
-
-        Button connectBtn = new Button();
-        
-
         scene.setRoot(g);
         primaryStage.show();
+
 
         //Close these when app closes
         //os.close();
