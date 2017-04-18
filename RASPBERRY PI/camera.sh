9a94c44312a038a251298118956f1f6416c848a0
Murat Kan <muratkan@Murats-MacBook-Pro.local>
2017-04-18 18:30:48 +0200

    uv4l installation

--- RASPBERRY PI/camera.sh
+++ RASPBERRY PI/camera.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-raspivid -o - -n -t 0 -w 640 -h 360 -fps 12 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8090}' :demux=h264
+uv4l --auto-video_nr --driver raspicam --encoding mjpeg --server-option '--port=8090' --driver raspicam --rotation 180 --width 352 --height 288
